0f2caa232eac98938cae5dda43a1dfff50113852
javierf39 <javi.freire@duocuc.cl>
2022-04-25 20:30:34 -0400

    models

--- ModuloGeneral/models.py
+++ ModuloGeneral/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    id_region = models.AutoField(primary_key=True, verbose_name="Id Region")
+    nombre_region = models.CharField(max_length=100, verbose_name="Nombre Region")
+
+class Comuna(models.Model):
+    id_comuna = models.AutoField(primary_key=True, verbose_name="Id Comuna")
+    nombre_comuna = models.CharField(max_length=100, verbose_name="Nombre comuna")
+    id_region = models.ForeignKey(Region, on_delete=models.CASCADE)
