295f8683317405049bb07d624c1b0c50dd7618c8
Benjamín Navarro <be.navarroa@duocuc.cl>
2022-04-28 05:21:10 -0400

    tab Yo

--- ModuloGeneral/static/js/menuPanelCliente.js
+++ ModuloGeneral/static/js/menuPanelCliente.js
@@ -1,14 +1,29 @@
 const sideMenu = document.querySelector("aside");
+const misMascotasTab = document.querySelector(".tabMisMascotas")
+const yoTab = document.querySelector(".tabPanelCliente")
+
+
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const yoBtn = document.querySelector("#yo-btn")
+const misMascotasBtn = document.querySelector("#misMascotas-btn")
 
 menuBtn.addEventListener('click', () =>{
     sideMenu.style.display = 'block';
-
 })
 
 closeBtn.addEventListener('click', () =>{
     sideMenu.style = 'display:none;';
+})
+
+yoBtn.addEventListener('click', () =>{
+    misMascotasTab.style = 'display:none;';
+    yoTab.style = 'display:block;'
+})
+
+misMascotasBtn.addEventListener('click', () =>{
+    yoTab.style = 'display:none;'
+    misMascotasTab.style = 'display:block;';
 })
 
 function enviarAnchoWeb(){
