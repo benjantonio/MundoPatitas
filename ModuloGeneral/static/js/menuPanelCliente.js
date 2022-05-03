b36818506fe3830345a45f5f0818ef8b82846d76
Benjamín Navarro <be.navarroa@duocuc.cl>
2022-05-03 03:16:40 -0400

    mejora menu general + mejora panel cliente

--- ModuloGeneral/static/js/menuPanelCliente.js
+++ ModuloGeneral/static/js/menuPanelCliente.js
@@ -3,33 +3,34 @@ const misMascotasTab = document.querySelector("#tabMisMascotas")
 const yoTab = document.querySelector("#tabYo")
 
 
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
+const menuBtnCli = document.querySelector("#menu-btncli");
+const closeBtnCli = document.querySelector("#close-btncli");
 const yoBtn = document.querySelector("#yo-btn")
 const misMascotasBtn = document.querySelector("#misMascotas-btn")
 
-menuBtn.addEventListener('click', () =>{
-    sideMenu.style.display = 'block';
-})
+menuBtnCli.addEventListener('click', () =>{
+    sideMenu.style = 'display:block;';
+});
 
-closeBtn.addEventListener('click', () =>{
+closeBtnCli.addEventListener('click', () =>{
     sideMenu.style = 'display:none;';
-})
+});
 
 yoBtn.addEventListener('click', () =>{
     misMascotasTab.style = 'display:none;';
     yoTab.style = 'display:block;'
-})
+});
 
 misMascotasBtn.addEventListener('click', () =>{
     yoTab.style = 'display:none;'
     misMascotasTab.style = 'display:block;';
-})
+});
 
 function enviarAnchoWeb(){
     let ancho = document.documentElement.clientWidth;
 
     if (ancho >=751){
+        console.log("ANCHO ES MAYOR A 750")
         sideMenu.style.display = 'block';
     }
 
