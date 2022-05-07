ff46a578a66712f1ef5d061c2bf7cc8dcd5b9308
Benjamín Navarro <be.navarroa@duocuc.cl>
2022-05-06 21:02:22 -0400

    Incorporación de Comentarios y Orden en el Código

--- ModuloGeneral/static/js/menuPanelCliente.js
+++ ModuloGeneral/static/js/menuPanelCliente.js
@@ -1,30 +1,54 @@
-const sideMenu = document.querySelector("aside");
-const misMascotasTab = document.querySelector("#tabMisMascotas")
-const yoTab = document.querySelector("#tabYo")
+/* ZONA RESPONSIVA */
+    const sideMenu = document.querySelector("aside");
 
+    /* Botones Abrir / Cerrar Menú */
+    const menuBtnCli = document.querySelector("#menu-btncli");
+    const closeBtnCli = document.querySelector("#close-btncli");
 
-const menuBtnCli = document.querySelector("#menu-btncli");
-const closeBtnCli = document.querySelector("#close-btncli");
-const yoBtn = document.querySelector("#yo-btn")
-const misMascotasBtn = document.querySelector("#misMascotas-btn")
+/* ZONA TABS (Botones del Menú y las vistas)*/
+    const yoBtn = document.querySelector("#yo-btn")
+    const yoTab = document.querySelector("#tabYo")
+    const misMascotasBtn = document.querySelector("#misMascotas-btn")
+    const misMascotasTab = document.querySelector("#tabMisMascotas")
+    const historialCitasBtn = document.querySelector("#historialCitas-btn")
+    const historialCitasTab = document.querySelector("#tabHistorialCitas")
 
-menuBtnCli.addEventListener('click', () =>{
-    sideMenu.style = 'display:block;';
-});
+/* ======================================================================== */
 
-closeBtnCli.addEventListener('click', () =>{
-    sideMenu.style = 'display:none;';
-});
+/* BOTONES RESPONSIVOS */
 
-yoBtn.addEventListener('click', () =>{
-    misMascotasTab.style = 'display:none;';
-    yoTab.style = 'display:block;'
-});
+    /* Botón Abrir Menú */
+    menuBtnCli.addEventListener('click', () =>{
+        sideMenu.style = 'display:block;';
+    });
 
-misMascotasBtn.addEventListener('click', () =>{
-    yoTab.style = 'display:none;'
-    misMascotasTab.style = 'display:block;';
-});
+    /* Botón Cerrar Menú */
+    closeBtnCli.addEventListener('click', () =>{
+        sideMenu.style = 'display:none;';
+    });
+
+/* MOSTRAR/OCULTAR TABS PANEL CLIENTE*/
+
+    /* Ventana "Yo" */
+    yoBtn.addEventListener('click', () =>{
+        yoTab.style = 'display:block;'
+        misMascotasTab.style = 'display:none;';
+        historialCitasTab.style = 'display:none;';
+    });
+
+    /* Ventana "Mis Mascotas" */
+    misMascotasBtn.addEventListener('click', () =>{
+        misMascotasTab.style = 'display:block;';
+        yoTab.style = 'display:none;'
+        historialCitasTab.style = 'display:none;';
+    });
+
+    /* Ventana "Historial Citas" */
+    historialCitasBtn.addEventListener('click', () =>{
+        historialCitasTab.style = 'display:block;';
+        misMascotasTab.style = 'display:none;';
+        yoTab.style = 'display:none;'
+    });
 
 function enviarAnchoWeb(){
     let ancho = document.documentElement.clientWidth;
