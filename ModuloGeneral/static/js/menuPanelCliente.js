2180a8f804fea2bf2b5a56f08e59d1e1a6a70c3b
Benjamín Navarro <be.navarroa@duocuc.cl>
2022-05-08 01:32:59 -0400

    creacion detalle historial

--- ModuloGeneral/static/js/menuPanelCliente.js
+++ ModuloGeneral/static/js/menuPanelCliente.js
@@ -1,31 +1,42 @@
-/* ZONA RESPONSIVA */
+/* VARIABLES DE MENU RESPONSIVO */
     const sideMenu = document.querySelector("aside");
 
     /* Botones Abrir / Cerrar Menú */
     const menuBtnCli = document.querySelector("#menu-btncli");
     const closeBtnCli = document.querySelector("#close-btncli");
 
-/* ZONA TABS (Botones del Menú y las vistas)*/
+/* VARIABLES PARA ABRIR TABS ( Son los botones del Menú y las vistas de los tabs)*/
+    /* Yo */
     const yoBtn = document.querySelector("#yo-btn")
     const yoTab = document.querySelector("#tabYo")
+
+    /* Mis Mascotas */
     const misMascotasBtn = document.querySelector("#misMascotas-btn")
     const misMascotasTab = document.querySelector("#tabMisMascotas")
+
+    /* Historial Citas */
     const historialCitasBtn = document.querySelector("#historialCitas-btn")
     const historialCitasTab = document.querySelector("#tabHistorialCitas")
 
-/* ======================================================================== */
+    const detalleCitaBtn = document.querySelector("#detalleCita-btn")
+    const detalleCita = document.querySelector("#detalleCita") /* ventana emergente del detalle de 1 cita */
+    const contDetalleCita = document.querySelector("#contenedorCita")
+
+/* =================================================================================================================== */
 
 /* BOTONES RESPONSIVOS */
 
     /* Botón Abrir Menú */
     menuBtnCli.addEventListener('click', () =>{
-        sideMenu.style = 'display:block;';
+        sideMenu.style = 'display:flex;';
     });
 
     /* Botón Cerrar Menú */
     closeBtnCli.addEventListener('click', () =>{
         sideMenu.style = 'display:none;';
     });
+
+/* ============================================================================= */
 
 /* MOSTRAR/OCULTAR TABS PANEL CLIENTE*/
 
@@ -49,6 +60,22 @@
         misMascotasTab.style = 'display:none;';
         yoTab.style = 'display:none;'
     });
+
+/* ============================================================================= */
+
+/* FUNCIONES */
+
+    detalleCitaBtn.addEventListener('click', () =>{
+        detalleCita.setAttribute("style", "opacity: 1; display:flex; ");
+        contDetalleCita.setAttribute("style", "opacity: 1; display:flex; top:40%; transition: all 600ms ease;");
+    });
+
+     /* Ocultar detalle Cita con click fuera */
+    detalleCita.addEventListener('click', () =>{
+        detalleCita.setAttribute("style", "opacity: 0; display:none;");
+        contDetalleCita.setAttribute("style", "opacity: 0; display:none;");
+    });
+
 
 function enviarAnchoWeb(){
     let ancho = document.documentElement.clientWidth;
