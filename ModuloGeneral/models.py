e4972d983a4fe15465b1f5f302752a656f857933
Benjamín Navarro <be.navarroa@duocuc.cl>
2022-05-19 04:33:20 -0400

    correccion modelo2

--- ModuloGeneral/models.py
+++ ModuloGeneral/models.py
@@ -147,7 +147,7 @@ class Veterinario(models.Model):
 class CitaDisponible(models.Model):
     id_cita = models.AutoField(primary_key=True)
     fecha = models.DateField(auto_now_add=False, auto_now=False)
-    hora = models.DateField(auto_now_add=False, auto_now=False)
+    hora = models.TimeField(auto_now_add=False, auto_now=False)
     id_veterinario = models.ForeignKey(Veterinario, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -167,7 +167,7 @@ class CitaConcluida(models.Model):
     # id_cita = models.OneToOneField(CitaMedica, on_delete=models.CASCADE)
     tratamiento = models.CharField(max_length=600)
     comentario = models.CharField(max_length=1000)
-    valoracion = models.IntegerField()
+    valoracion = models.IntegerField(default=0)
 
     def __str__(self):
         return self.id_cita
